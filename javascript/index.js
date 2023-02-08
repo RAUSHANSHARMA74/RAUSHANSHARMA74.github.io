875d92f90ebbe2f3bb9bac7fa209f40b649326c0
Raushan <raushansharma7462@gmail.com>
2023-02-08 17:02:35 +0530

    done

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+// let homeSection = document.getElementById("#home-section")
+
+// let home = document.getElementById("#home")
+// home.addEventListener("click", ()=>{
+//     homeSection.style.visibility = "hidden";
+
+// })
